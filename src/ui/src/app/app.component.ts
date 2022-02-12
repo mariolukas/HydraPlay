426f4c39e02451b2f9d254517c6e859876bbc336
Mario Lukas <info@fabscan.org>
2022-02-12 22:53:42 +0100

    reafactoring (frontend): removed client not connected message from app controler

--- src/ui/src/app/app.component.ts
+++ src/ui/src/app/app.component.ts
@@ -32,11 +32,12 @@ export class AppComponent implements OnInit, OnDestroy {
 
       });
 
+      /*
       if (!clientsConnected){
         this.notificationService.modalInfo("No Snapclient connected!");
       } else {
         this.notificationService.dismissInfo();
-      }
+      }*/
 
       this.groups = groups;
     })
