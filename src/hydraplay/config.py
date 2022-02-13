fa2457197310943a7ac004b3860351d12e51c859
Mario Lukas <info@fabscan.org>
2022-02-13 19:42:08 +0100

    fix (hydraplay server): added validation check for config file

--- src/hydraplay/config.py
+++ src/hydraplay/config.py
@@ -7,16 +7,19 @@ class Config:
     def __init__(self, file_name):
         self.logger = logging.getLogger(__name__)
         self.file_name = file_name
-        self.content = self.load_json(file_name)
-        self.content = YAMLobj(self.content)
+        try:
+            self.content = self.load_json(file_name)
+            self.content = YAMLobj(self.content)
+        except Exception as error:
+            self.logger.error("Config file is not valid JSON, please check your hydraplay config again.")
+            return None
 
     def load_json(self, file):
         self.logger.debug("Loading config file.")
         with open(file) as json_data_file:
-            data = json.load(json_data_file)
-            # fill config with default values when not set
-            self.logger.debug("Checking for valid config values.")
-
+                data = json.load(json_data_file)
+                # fill config with default values when not set
+                self.logger.debug("Config file is valid.")
         return data
 
     def save_json(self, file_name=None):
--- src/hydraplay/server/HydraServer.py
+++ src/hydraplay/server/HydraServer.py
@@ -17,36 +17,45 @@ from tornado.platform.asyncio import AnyThreadEventLoopPolicy
 class HydraServer:
 
     def __init__(self, configFile):
-
-        asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
-        self.system_exit = SystemExit()
-        self.logger = logging.getLogger(__name__)
-        self.logger.setLevel(logging.DEBUG)
-        self.config = Config(configFile)
-        self.static_files = str(Path(__file__).resolve().parent) + "/static/"
-        self.logger.debug(self.static_files)
-        self.server_port = self.config.content['hydraplay']['port']
+        self.mopidy_sercice = None
         self.webserver = None
+        self.snapcast_service = None
+        self.exit_on_error = False
 
-        self.mopidy_sercice = MopidyPoolService(self.config.content)
-        self.snapcast_service = SnapCastService(self.config.content)
+        try:
+            self.config = Config(configFile)
+            asyncio.set_event_loop_policy(AnyThreadEventLoopPolicy())
+            self.system_exit = SystemExit()
+            self.logger = logging.getLogger(__name__)
+            self.logger.setLevel(logging.DEBUG)
 
+            self.static_files = str(Path(__file__).resolve().parent) + "/static/"
+            self.logger.debug(self.static_files)
+            self.server_port = self.config.content['hydraplay']['port']
+
+            self.mopidy_sercice = MopidyPoolService(self.config.content)
+            self.snapcast_service = SnapCastService(self.config.content)
+        except:
+            self.exit_on_error = True
+            self.shutdown()
 
     def run(self):
-        self.logger.info("Hydraplay Server started.")
-        self.mopidy_sercice.start()
-        time.sleep(2)
-        self.snapcast_service.start()
+        if not self.exit_on_error:
+            self.logger.info("Hydraplay Server started.")
+            self.mopidy_sercice.start()
+            time.sleep(2)
+            self.snapcast_service.start()
 
-        self.logger.debug("Server listening on port {0}".format(self.server_port))
-        self.webserver = self.routes()
-        self.webserver.listen(self.server_port)
-        tornado.ioloop.IOLoop.instance().start()
+            self.logger.debug("Server listening on port {0}".format(self.server_port))
+            self.webserver = self.routes()
+            self.webserver.listen(self.server_port)
+            tornado.ioloop.IOLoop.instance().start()
 
     def shutdown(self):
        #self.snapcast_service.stop()
        #self.snapcast_service.join()
-       self.mopidy_sercice.stop()
+       if self.mopidy_sercice is not None:
+            self.mopidy_sercice.stop()
        #self.mopidy_sercice.join()
        ioloop = tornado.ioloop.IOLoop.current()
        ioloop.add_callback(ioloop.stop)
