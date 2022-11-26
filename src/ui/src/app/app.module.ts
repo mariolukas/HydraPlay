549f56e827f241822404e6b1ac1b0fb20824f14d
Mario Lukas <info@fabscan.org>
2022-11-26 08:03:18 +0100

    chore (package.json): fixed angular version an package.json

--- src/ui/src/app/app.module.ts
+++ src/ui/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { AppComponent } from './app.component';
 import { PlayerComponent } from './components/player/player.component';
 import { NoopAnimationsModule} from '@angular/platform-browser/animations';
 
+
 import {MatSliderModule} from "@angular/material/slider";
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
